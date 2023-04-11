73b26e2723cf792538f10fdbf60819c963fad2bb
Lenard Frommelt <lenny09918050@gmail.com>
2023-04-11 09:24:53 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -3,11 +3,13 @@ module.exports = {
         return {
             "mustache": "^4.2.0",
             "secret-manager-client": "^0.0.6",
-            "secret-manager-crypto-utils": "^0.2.0",
+            "secret-manager-crypto-utils": "^0.3.0",
             "vanillaqr": "^1.0.0",
             "qr-scanner": "^1.4.2",
             "thingy-network-base": "^0.0.4",
-            "thingy-byte-utils": "^0.0.7",
+            "thingy-byte-utils": "^0.0.8",
+            "thingy-post-rpc-client": "^0.0.1",
+            "thingy-rpc-authmaster-client": "^0.0.3",
             "thingy-session-utils": "^0.0.3",
             "validatabletimestamp": "^0.0.1"
         }
