a12b659629d832b126fdfee607a1f87d4a5972e3
Lenard Frommelt <lenny09918050@gmail.com>
2024-12-11 15:11:52 +0300

    1

--- sourceInfo.js
+++ sourceInfo.js
@@ -3,17 +3,15 @@ module.exports = {
         return {
             "navhandler": "^0.0.4",
             "mustache": "^4.2.0",
-            "secret-manager-client": "^0.0.6",
-            "secret-manager-crypto-utils": "^0.3.0",
+            "secret-manager-crypto-utils": "^0.3.1",
             "vanillaqr": "^1.0.0",
             "qr-scanner": "^1.4.2",
-            "thingy-crypto-node": "^0.0.1",
-            "thingy-network-base": "^0.0.4",
+            "thingy-crypto-node": "^0.0.2",
             "thingy-byte-utils": "^0.0.8",
-            "thingy-post-rpc-client": "^0.0.6",
+            "thingy-post-rpc-client": "^0.0.7",
             "thingy-rpc-authmaster-client": "^0.0.9",
-            "thingy-session-utils": "^0.0.5",
-            "validatabletimestamp": "^0.0.1"
+            "thingy-session-utils": "^0.0.6",
+            "validatabletimestamp": "^0.0.4"
         }
     }
 }
